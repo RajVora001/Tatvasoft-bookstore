4d61f8e51d3b98d46bcbd226dd18996b19766836
RajVora001 <voraraj726@gecg28.ac.in>
2023-08-07 01:45:06 +0530

    third commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,6 +3,52 @@ import { useState, useEffect } from 'react';
 
 const Login = () => {
 
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [formErrors, setFormErrors] = useState({
+    email: '',
+    password: '',
+  });
+
+  const validateForm = () => {
+    const { email, password } = formData;
+    let errors = {};
+
+    // Validation rules, add more rules as needed
+    if (!email.trim()) {
+      errors.email = 'Email is required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email)) {
+      errors.email = 'Invalid email address';
+    } else {
+      errors.email = '';
+    }
+
+    if (!password.trim()) {
+      errors.password = 'Password is required';
+    } else if (password.length < 6) {
+      errors.password = 'Password must be at least 6 characters long';
+    } else {
+      errors.password = '';
+    }
+
+    setFormErrors(errors);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
 
@@ -21,21 +67,23 @@ const Login = () => {
               <li>Save multiple shipping addresses</li>
               <li>View and track orders and more</li>
             </ol>
-            <button className=' w-56 h-11 bg-red-500 text-white text-lg rounded mt-40 mb-20' >Create an Account</button>
+            <button className=' w-56 h-11 bg-red-500 text-white text-lg rounded mt-40 mb-20 hover:bg-red-600 font-semibold' >Create an Account</button>
           </div>
-          <div className='flex flex-col mr-52'>
+          <form onSubmit={handleSubmit} className='flex flex-col mr-52'>
             <span className=' text-xl mb-5' >Registered Customer</span>
             <div className=' w-5/6 h-px bg-slate-500 mb-5' />
 
             <span className=' text-sm mt-5 mb-3'>Email Address*</span>
-            <input type="email" name="email" className=' w-80 h-10 border-2 border-slate-400 rounded-md' />
+            <input type="email" name="email" value={formData.email} onChange={handleChange} className=' w-80 h-10 border-2 border-slate-400 rounded-md' />
+            {formErrors.email && <span>{formErrors.email}</span>}
 
             <span className=' text-sm mt-5 mb-3'>Password*</span>
-            <input type="password" name="password" className=' w-80 h-10 border-2 border-slate-400 rounded-md' />
+            <input type="password" name="password" value={formData.password} onChange={handleChange} className=' w-80 h-10 border-2 border-slate-400 rounded-md' />
+            {formErrors.password && <span>{formErrors.password}</span>}
+            
+            <button type="submit" className=' bg-red-500 w-24 h-11 rounded text-white text-lg mt-14 mb-20 hover:bg-red-600 font-semibold'>Login</button>
 
-            <button type="submit" className=' bg-red-500 w-24 h-11 rounded text-white text-lg mt-14 mb-20'>Login</button>
-
-          </div>
+          </form>
         </div>
       </div>
 
